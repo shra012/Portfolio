ba8fb84a89cc8ea5c1788d70d9fdf8c7d8327390
shra012 <shravan_fisher@yahoo.ca>
2025-07-07 17:54:09 +0000

    added changes

--- frontend/src/components/canvas/Computers.jsx
+++ frontend/src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf');
+  const computer = useGLTF(`${import.meta.env.BASE_URL}desktop_pc/scene.gltf`);
 
   return (
     <mesh>
